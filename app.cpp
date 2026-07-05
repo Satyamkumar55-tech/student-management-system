3416068f52540761edd11e2096eda14353dedee6
Satyamkumar Singh <satyamranjeet2006@gmail.com>
2026-07-05 19:10:54 +0530

    Add initial implementation of Student struct and main function

---
+++ app.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+    struct Student
+    {
+        int s_id;
+        string name;
+        string department;
+        double cgpa;
+        string address;
+    };
+
+    vector<Student> students;
+
+int main(){
+
+}
+    
+
